2ef2582576a8cf0624339c7fa757ae3290496592
ART89-prog <72185684+ART89-prog@users.noreply.github.com>
2022-11-25 16:46:34 +0300

    fix 8

--- js/scripts.js
+++ js/scripts.js
@@ -101,6 +101,19 @@ $(() => {
       });
 
 
+      $(".my_audio").trigger('load');
+
+
+      function play_audio(task) {
+        if(task == 'play'){
+             $(".my_audio").trigger('play');
+        }
+        if(task == 'stop'){
+             $(".my_audio").trigger('pause');
+             $(".my_audio").prop("currentTime",0);
+        }
+      }
+
 
       $('body').on('click', '.modal_btn', function (e) {
         e.preventDefault()
